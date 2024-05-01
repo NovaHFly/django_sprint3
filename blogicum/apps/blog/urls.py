8d49e7e4a5f2ff5b9e46a049d86d97bc7c52dd39
NovaHFly <mugiviska@gmail.com>
2024-05-01 09:40:23 +0300

    fix: Make imports in apps urls absolute

--- blogicum/apps/blog/urls.py
+++ blogicum/apps/blog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from . import views
+from blog import views
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts/<int:id_>/', views.post_detail, name='post_detail'),
+    path('posts/<int:id>/', views.post_detail, name='post_detail'),
     path(
         'category/<slug:category_slug>/',
         views.category_posts,
