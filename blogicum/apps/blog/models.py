01a927b2a593a26bfe4fe419c31bdbc350ec6315
NovaHFly <mugiviska@gmail.com>
2024-05-14 15:14:29 +0300

    feat: Create models for blog

--- blogicum/apps/blog/models.py
+++ blogicum/apps/blog/models.py
@@ -1,18 +1,32 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-from core.models import PublishedModel
+from core.models import (
+    DateCreatedModel,
+    PublishedModel,
+)
+
+User = get_user_model()
 
 
-class Category(PublishedModel):
+class Category(PublishedModel, DateCreatedModel):
+    """Category of posts by the same theme.
+
+    Attributes:
+        title (CharField[256]): name of the category.
+        slug (SlugField): slug of the category.
+        description (TextField): verbose description of the category.
+    """
+
     title = models.CharField(
-        max_length=64,
+        max_length=256,
         verbose_name='Название',
     )
     slug = models.SlugField(
-        max_length=64,
         verbose_name='Слаг',
         unique=True,
     )
+    description = models.TextField(verbose_name='Описание')
 
     class Meta:
         verbose_name = 'Категория'
@@ -22,24 +36,56 @@ class Category(PublishedModel):
         return self.title
 
 
-class BlogPost(PublishedModel):
-    date = models.DateField(verbose_name='Дата публикации')
+class Location(PublishedModel, DateCreatedModel):
+    """Some landmark.
+
+    Attributes:
+        name (CharField[256]): name of the landmark.
+    """
+
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+    )
+
+    class Meta:
+        verbose_name = 'Локация'
+        verbose_name_plural = 'Локации'
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Post(PublishedModel, DateCreatedModel):
+    title = models.CharField(
+        max_length=256,
+        verbose_name='Заголовок',
+    )
+    text = models.TextField(
+        verbose_name='Содержание',
+    )
+    pub_date = models.DateTimeField(verbose_name='Дата')
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор',
+    )
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='posts',
+        verbose_name='Локация',
+    )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
-        verbose_name='Категория',
+        null=True,
         related_name='posts',
-        null=True,
-    )
-    location = models.CharField(
-        max_length=64,
-        verbose_name='Локация',
-        null=True,
-        blank=True,
-    )
-    text = models.TextField(
-        max_length=512,
-        verbose_name='Текст публикации',
+        verbose_name='Категория',
     )
 
     class Meta:
@@ -47,4 +93,4 @@ class BlogPost(PublishedModel):
         verbose_name_plural = 'Публикации'
 
     def __str__(self) -> str:
-        return f'{self.date} - {self.location}'
+        return f'{self.pub_date} - {self.title}'
