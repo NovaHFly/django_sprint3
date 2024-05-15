52f152110c6a75f3a9deb6f6cff6a640fa6e9229
NovaHFly <mugiviska@gmail.com>
2024-05-15 10:38:03 +0300

    feat: Add hints to model fields

--- blogicum/apps/blog/models.py
+++ blogicum/apps/blog/models.py
@@ -25,6 +25,8 @@ class Category(PublishedModel, DateCreatedModel):
     slug = models.SlugField(
         verbose_name='Идентификатор',
         unique=True,
+        help_text='Идентификатор страницы для URL; разрешены'
+        ' символы латиницы, цифры, дефис и подчёркивание.',
     )
     description = models.TextField(verbose_name='Описание')
 
@@ -77,6 +79,8 @@ class Post(PublishedModel, DateCreatedModel):
     )
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
+        help_text='Если установить дату и время в будущем'
+        ' — можно делать отложенные публикации.'
     )
 
     author = models.ForeignKey(
--- blogicum/apps/core/models.py
+++ blogicum/apps/core/models.py
@@ -4,7 +4,8 @@ from django.db import models
 # Create your models here.
 class PublishedModel(models.Model):
     is_published = models.BooleanField(
-        default=True, verbose_name='Опубликовано'
+        default=True, verbose_name='Опубликовано',
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
     )
 
     class Meta:
