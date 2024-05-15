6ffc464a225e2d4714ae3e6df2cc82afea475a77
NovaHFly <mugiviska@gmail.com>
2024-05-15 13:18:41 +0300

    feat: Update views in blog app

--- blogicum/apps/blog/views.py
+++ blogicum/apps/blog/views.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
 
-from blog.models import BlogPost, Category
+import blog.models
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -10,11 +12,28 @@ def index(request: HttpRequest) -> HttpResponse:
     Args:
         request (HttpRequest): Request received from the user.
     """
-    posts = BlogPost.objects.filter(is_published=True).select_related(
-        'category'
-    )
     template = 'blog/index.html'
-    context = {'post_list': reversed(posts)}
+    # - Cache all foreign keys (as all are needed)
+    # - Exclude all posts which are either:
+    #   1. Not published
+    #   2. From a not published category
+    #   3. Published at a date and time later than now
+    # - Order resulting posts by publication date descending
+    # - Get 5 latest posts
+    posts = (
+        blog.models.Post.objects.select_related(
+            'category',
+            'location',
+            'author',
+        )
+        .filter(
+            is_published=True,
+            category__is_published=True,
+            pub_date__lte=datetime.now(),
+        )
+        .order_by('-pub_date')
+    )[:5]
+    context = {'post_list': posts}
     return render(request, template, context)
 
 
@@ -26,10 +45,24 @@ def post_detail(request: HttpRequest, id: int) -> HttpResponse:  # noqa: A002
         id (int): Post id.
     """
     template = 'blog/detail.html'
-
+    # - Cache all foreign keys (as all are needed)
+    # - Exclude all posts which are either:
+    #   1. Not published
+    #   2. From a not published category
+    #   3. Published at a date and time later than now
+    # - Get post which id is equal to [id]
+    # - If no post found, return error 404
     required_post = get_object_or_404(
-        BlogPost.objects.all(),
-        pk=id
+        blog.models.Post.objects.select_related(
+            'category',
+            'location',
+            'author',
+        ).filter(
+            is_published=True,
+            category__is_published=True,
+            pub_date__lte=datetime.now(),
+        ),
+        pk=id,
     )
 
     context = {'post': required_post}
@@ -41,10 +74,28 @@ def category_posts(request: HttpRequest, category_slug: str) -> HttpResponse:
 
     Args:
         request (HttpRequest): Request received from the user.
-        category_slug (str): Category name.
+        category_slug (str): Category identifier.
     """
     template = 'blog/category.html'
-    category = Category.objects.get(slug=category_slug)
-    posts = category.posts.all()
-    context = {'category': category.title, 'post_list': reversed(posts)}
+    # Get category object.
+    # If category is not published or
+    #   no match found for category_slug
+    #   return error 404
+    category = get_object_or_404(
+        blog.models.Category.objects.filter(is_published=True),
+        slug=category_slug,
+    )
+    # Get all posts from this category, exclude those which are:
+    # 1. Not published
+    # 2. Published at a date and time later than now
+    # Order them by publication date descending
+    posts = (
+        category.posts.select_related(
+            'location',
+            'author',
+        )
+        .filter(is_published=True, pub_date__lte=datetime.now())
+        .order_by('-pub_date')
+    )
+    context = {'category': category.title, 'post_list': posts}
     return render(request, template, context)
