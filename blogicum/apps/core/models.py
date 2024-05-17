f21df8e5cc7d6dcc8787df96723e5c621296f09b
NovaHFly <mugiviska@gmail.com>
2024-05-17 11:18:58 +0300

    fix: Remove django standart comments

--- blogicum/apps/core/models.py
+++ blogicum/apps/core/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 class PublishedModel(models.Model):
     is_published = models.BooleanField(
         default=True, verbose_name='Опубликовано',
